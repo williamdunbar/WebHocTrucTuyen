e0f4f120b521a2ec149d5625598098dffcb7aacb
William <williamdunbar2000@gmail.com>
2021-11-02 01:09:15 +0700

    1_11_2021 Cap nhat template vao solution MVC

---
+++ WebHocTrucTuyen/Areas/Student/Controllers/StudentHomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebHocTrucTuyen.Areas.Student.Controllers
+{
+    public class StudentHomeController : Controller
+    {
+        // GET: Student/StudentHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ThongTinCaNhan()
+        {
+            return View();
+        }
+    }
+}
